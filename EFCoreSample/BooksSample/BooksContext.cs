a2a20aeee8294a2b7ccbe355c03b65da900d6b89
Mohammad Iqbal <Miqbal@d-shore.co.id>
2019-09-13 19:44:36 +0700

    first commit

---
+++ EFCoreSample/BooksSample/BooksContext.cs
@@ -0,0 +1,17 @@
+﻿using EFCoreSample.Intro;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreSample.BooksSample
+{
+    public class BooksContext : DbContext
+    {
+        private const string ConnectionString = @"Server=localhost;Database=learning;User Id=sa;Password=P@ssw0rd";
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
